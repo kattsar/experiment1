580aba10cbfd4181cf197c5e7a664c9bf599420d
Aikaterini Tsaroucha <k.tsaroucha@bcbl.eu>
2023-05-25 12:25:47 +0200

    trying

--- REPSWITCH_1_lastrun.py
+++ REPSWITCH_1_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.5),
-    on Μάιος 25, 2023, at 12:16
+    on Μάιος 25, 2023, at 12:23
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
